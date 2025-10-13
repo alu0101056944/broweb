21b1fdfdbe9d637473630671500b42cbc4316675
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2025-10-13 16:49:52 +0100

    New TextWithImage block.

--- src/AboutContent/AboutContent.tsx
+++ src/AboutContent/AboutContent.tsx
@@ -110,6 +110,92 @@ const ImageBlock: Block = {
   ],
 }
 
+const TextWithImageBlock: Block = {
+  slug: 'textWithImageBlock',
+  interfaceName: 'textWithImageBlock',
+  fields: [
+    {
+      name: 'imageUrl',
+      label: 'Image URL',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'altText',
+      label: 'Alt Text',
+      type: 'text',
+      required: true,
+      admin: {
+        description: 'Describe the image for screen readers and SEO.'
+      }
+    },
+    {
+      name: 'width',
+      label: 'Width of the image',
+      type: 'number',
+      required: false,
+      defaultValue: -1,
+      admin: {
+        description: 'If not specified the width will be as big as possible within it\'s container.',
+        placeholder: 'ejemplo: 128, 200, 764...',
+        step: 1,
+        position: 'sidebar',
+      }
+    },
+    {
+      name: 'height',
+      label: 'Height of the image',
+      type: 'number',
+      required: false,
+      defaultValue: -1,
+      admin: {
+        description: 'If not specified the height will be as big as possible within it\'s container.',
+        placeholder: 'ejemplo: 480, 501, 652...',
+        step: 1,
+        position: 'sidebar',
+      }
+    },
+    {
+      name: 'alignment',
+      label: 'Image Alignment',
+      type: 'select',
+      defaultValue: 'center',
+      options: [
+        {
+          label: 'Left',
+          value: 'left',
+        },
+        {
+          label: 'Right',
+          value: 'right',
+        },
+      ],
+      admin: {
+        position: 'sidebar',
+        description: 'Choose how to align the image relative to the text.',
+      }
+    },
+    {
+      name: 'richText',
+      type: 'richText',
+      editor: lexicalEditor({
+        admin: {
+          placeholder: 'Escribir aquí el contenido a mostrar...',
+        },
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          BoldFeature(),
+          ItalicFeature(),
+          UnderlineFeature(),
+          StrikethroughFeature(),
+          UnorderedListFeature(),
+          OrderedListFeature(),
+        ],
+      }),
+    },
+  ],
+}
+
 export const AboutContent: GlobalConfig = {
   slug: 'about-content',
 
@@ -125,6 +211,7 @@ export const AboutContent: GlobalConfig = {
       blocks: [
         RichTextBlock,
         ImageBlock,
+        TextWithImageBlock,
       ],
     },
   ],
