8ab9fd09a6a4d4d9e09e19482f87b488ef6d73fb
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2026-01-19 17:05:50 +0000

    Fixed bad hook for remote image dimensions on home global.

--- src/HomeContent/HomeContent.tsx
+++ src/HomeContent/HomeContent.tsx
@@ -1,8 +1,64 @@
-import { GlobalConfig } from 'payload'
+import { GlobalConfig, GlobalBeforeChangeHook } from 'payload'
 
 import { authenticated } from '../access/authenticated'
 
-import { TextPage, addRemoteImageDimensions } from '../TextPage/TextPage'
+import { TextPage, ContentBlockType } from '../TextPage/TextPage'
+
+import sharp from 'sharp'
+
+export const addRemoteImageDimensions: GlobalBeforeChangeHook = async ({ data, req }) => {
+  if (!data.textpageContent) {
+    return data
+  }
+
+  const updated_data = await Promise.all(
+    data.textpageContent.map(async (block: ContentBlockType) => {
+      if (
+        (block.blockType === 'textWithImageBlock' || block.blockType === 'imageBlock') &&
+        block.imageUrl
+      ) {
+        try {
+          const response = await fetch(block.imageUrl)
+
+          if (!response.ok) {
+            throw new Error(`Failed to fetch image. Status: ${response.status}`)
+          }
+
+          const imageBuffer = Buffer.from(await response.arrayBuffer())
+          const metadata = await sharp(imageBuffer).metadata()
+
+          return {
+            ...block,
+            imageDimensions: {
+              width: metadata.width,
+              height: metadata.height,
+            },
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            req.payload.logger.error(
+              `[TextWithImage Hook] Error getting dimensions for ${data.imageUrl}: ${error.message}`,
+            )
+          }
+
+          return {
+            ...block,
+            imageDimensions: {
+              width: null,
+              height: null,
+            },
+          }
+        }
+      }
+      return block
+    }),
+  )
+
+  return {
+    ...data,
+    content: { textpageContent: updated_data },
+  }
+}
 
 export const HomeContent: GlobalConfig = {
   slug: 'home',
