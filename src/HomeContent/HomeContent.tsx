84f54729da5d9fe82063e214a168aa5eddc0fea2
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2026-01-19 13:10:22 +0000

    Explicit access on update for home content added.

--- src/HomeContent/HomeContent.tsx
+++ src/HomeContent/HomeContent.tsx
@@ -1,11 +1,14 @@
 import { GlobalConfig } from 'payload'
 
+import { authenticated } from '../access/authenticated'
+
 import { TextPage, addRemoteImageDimensions } from '../TextPage/TextPage'
 
 export const HomeContent: GlobalConfig = {
   slug: 'home',
   access: {
     read: () => true,
+    update: authenticated,
   },
   admin: {
     group: 'Static Pages',
